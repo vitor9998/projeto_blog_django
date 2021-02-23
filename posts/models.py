c5c6df022d61989e7d30f37d4e3567c761b55d55
Vitor Oikawa <vitor9998@gmail.com>
2021-02-23 11:56:48 -0300

    Redimensionando imagens do model

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categorias.models import Categoria
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 #on_delete --> o que acontece quando deleto.
 #upload_to
@@ -14,9 +17,36 @@ class Post(models.Model):
     excerto_post = models.TextField(verbose_name='Excerto')
     categoria_post = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING,
                                        blank=True, null=True, verbose_name='Categoria')
-    imagem_post = models.ImageField(upload_to='post_img/&Y/%m/%d', blank=True,
+    imagem_post = models.ImageField(upload_to='post_img/%Y/%m/%d', blank=True,
                                     null=True, verbose_name='Imagem')
     publicado_post = models.BooleanField(default=False, verbose_name='Publicado')
 
     def __str__(self):
         return self.titulo_post
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.imagem_post:
+            self.resize_image(self.imagem_post.name, 800)
+
+    @staticmethod
+    def resize_image(img_name, new_width):
+        img_path = os.path.join(settings.MEDIA_ROOT, img_name)
+        img = Image.open(img_path)
+        width, height = img.size
+        new_height = round((new_width * height) / width)
+
+        if width <= new_width:
+            img.close()
+            return
+
+        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        new_img.save(
+            img_path,
+            optimize=True,
+            quality=60
+        )
+        new_img.close()
+
+
