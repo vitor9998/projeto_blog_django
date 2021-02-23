49b2f1520e8a0a4380575234d2e219ef6f7f1d35
Vitor Oikawa <vitor9998@gmail.com>
2021-02-23 12:00:09 -0300

    Desfazendo alterações do model

--- posts/models.py
+++ posts/models.py
@@ -24,29 +24,4 @@ class Post(models.Model):
     def __str__(self):
         return self.titulo_post
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        if self.imagem_post:
-            self.resize_image(self.imagem_post.name, 800)
-
-    @staticmethod
-    def resize_image(img_name, new_width):
-        img_path = os.path.join(settings.MEDIA_ROOT, img_name)
-        img = Image.open(img_path)
-        width, height = img.size
-        new_height = round((new_width * height) / width)
-
-        if width <= new_width:
-            img.close()
-            return
-
-        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
-        new_img.save(
-            img_path,
-            optimize=True,
-            quality=60
-        )
-        new_img.close()
-
 
